e64af11fb629630c0dda01876072dd432ff8c7f1
Ashish Kumar Senapati <ashishkrse@gmail.com>
2022-12-04 18:46:41 +0530

    added states

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import { IExpenseDetail } from "../../types";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-export default function ExpenseItem(props: IExpenseDetail) {
+export default function ExpenseItem(pro: IExpenseDetail) {
+  const [title , setTitle] = useState(pro.title);
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={pro.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${pro.amount}</div>
       </div>
+      <button onClick={clickHandler}>Edit</button>
     </Card>
   );
 }

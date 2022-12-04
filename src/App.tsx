a5cfaec739178466e3e00296fa7e8b58a24570fe
Ashish Kumar Senapati <ashishkrse@gmail.com>
2022-12-04 21:22:19 +0530

    added new form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -38,6 +39,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   )

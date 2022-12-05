5947be214d70d97603db8305c098236488780fce
Ashish Kumar Senapati <ashishkrse@gmail.com>
2022-12-05 14:10:17 +0530

    added expense filter

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(props: any) {
-  const expenses  = props.items;
+  const expenses = props.items;
+  const [filteredYear, setFilteredYear] = useState("");
+
+  const onSelectYear = (year: any) => {
+    setFilteredYear(year);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter selected={filteredYear} onSelectYear={onSelectYear} />
+      <Card className="expenses">
+        <ExpenseItem
+          id={expenses[0].id}
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          id={expenses[1].id}
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+        <ExpenseItem
+          id={expenses[2].id}
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        />
+      </Card>
+    </div>
   );
 }

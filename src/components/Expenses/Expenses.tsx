f1e6a51d688f49982c3c6189cab74e9f6d62cfdb
Ashish Kumar Senapati <ashishkrse@gmail.com>
2022-12-05 19:36:59 +0530

    updated filter check

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -6,18 +6,15 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 export default function Expenses(props: any) {
-  const originalExpenses = props.items;
-  const [expenses, setExpenses] = useState(props.items);
   const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear: any) => {
     setFilteredYear(selectedYear);
-    const filteredExpenses = originalExpenses.filter(
-      (expense: any) =>
-        new Date(expense.date).getFullYear() === Number(selectedYear)
-    );
-    setExpenses(filteredExpenses);
   };
+  const filteredExpenses = props.items.filter(
+    (expense: any) =>
+      new Date(expense.date).getFullYear().toString() === filteredYear
+  );
 
   return (
     <div>
@@ -26,7 +23,7 @@ export default function Expenses(props: any) {
           selected={filteredYear}
           onSelectYear={filterChangeHandler}
         />
-        {expenses.map((expense: any) => (
+        {filteredExpenses.map((expense: any) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}

4888111fce2664a7cd024830c25b376272956c09
Ashish Kumar Senapati <ashishkrse@gmail.com>
2022-12-05 15:41:40 +0530

    fixed filter expenses css

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -13,28 +13,32 @@ export default function Expenses(props: any) {
   };
 
   return (
-    <div>
+    <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onSelectYear={onSelectYear} />
-      <Card className="expenses">
-        <ExpenseItem
-          id={expenses[0].id}
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          id={expenses[1].id}
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          id={expenses[2].id}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-      </Card>
-    </div>
+      <ExpenseItem
+        id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </Card>
   );
 }
